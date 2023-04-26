3276962163fcb32f31ad858b31fda4edcc9345a2
jpatidar9826 <jpatidar9826@gmail.com>
2023-04-26 23:54:13 +0530

    set index

--- index.js
+++ index.js
@@ -1,24 +1,140 @@
+require("dotenv").config();
+const express = require("express");
+const { Client } = require("twitter-api-sdk");
 const rwClient = require("./twitterClient.js");
 const CronJob = require("cron").CronJob;
+const axios = require("axios");
+const { chromium } = require('playwright');
+var fs = require("fs");
+const sarcReply = require('./sarcReply.js');
+//1615407323295657984 1605229092789682178 1650084228531642368 1615406967438323713
+var ScinceID;
+const client = new Client(process.env.BEARER_TOKEN);process.env.BEARER_TOKEN
 
-const tweet = async () => {
-    try {
+const app = express();
 
-      const mediaIds = await Promise.all([rwClient.v1.uploadMedia('./example.png')]);
-      await rwClient.v1.reply('reply to previously created tweet.',"1602579498062426112",{ media_ids: mediaIds });
+async function main() {
+  ScinceID = await fs.readFileSync('./store.txt', 'utf8');
+  const response = await client.tweets.usersIdMentions("1332268941159194626", {
+    since_id: [ScinceID],
+  });
 
-    } catch (e) {
-        console.error(e)
+  console.log(response.data);
+  if (response.meta.result_count > 0) {
+    console.log("yes");
+    for (var i = 0; i < response.meta.result_count; i++) {
+      //ScinceID = response.data[i].id;
+      fs.writeFileSync('./store.txt', response.data[i].id, 'utf8');
+      const temp = finder(response.data[i].id);
     }
+  } else {
+    console.log("no");
+  }
 }
 
-const job = new CronJob("*/1 * * * *", () => {
-    console.log('cron job starting!');
-//     tweet()
-});
-//
-console.log("at job");
-//job.start();
- tweet();
+const finder = async (id) => {
+  const finderResponse = await client.tweets
+    .findTweetsById({
+      ids: [id],
+      expansions: ["referenced_tweets.id"],
+    })
+    .then((res) => {
+      if (res.data[0].text.includes('joke')) {
+        joke(id);
+      } else if (res.data[0].text.includes('reply')) {
+          sarcReply(res.data[0].referenced_tweets[0].id, id);
+      }else {
+        screenshot(res.data[0].referenced_tweets[0].id, id);
+      }
+    });
+  console.log(finderResponse);
+};
 
-// const tweets = await client.v2.tweets(['20', '141']);
+const joke = async (id) => {
+  axios
+    .get(
+      "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist&type=single"
+    )
+    .then(async (res) => {
+      //tweet(res.data.joke, item.id)
+      try {
+        await rwClient.v1.reply(res.data.joke,id);
+        console.log("joke sent");
+      } catch (error) {
+        console.log("error at joke: "+ error);
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+const screenshot = async (ssid, id) => {
+  var link = "https://twitter.com/AlpheriorKeys/status/".concat(ssid);
+  var filepath = id + "example.png";
+  const browser = await chromium.launch().catch((res) => {
+    console.log(res);
+    return;
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto(link,{ waitUntil: "networkidle" ,waitNetworkIdle: true,timeout : 60000 });
+  await page.waitForLoadState('networkidle');
+  await page.waitForSelector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-jxzhtn.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > section > div > div > div:nth-child(1) > div > div > article',{ timeout: 10000 });
+
+  // Get the bounding box of the div element
+  const divBoundingBox = await page.$eval('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-jxzhtn.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > section > div > div > div:nth-child(1) > div > div > article', el => el.getBoundingClientRect());
+
+  // Take a screenshot of the div element and save it to a file
+  await page.screenshot({
+    path: filepath,
+    clip: divBoundingBox,
+  });
+
+  await browser.close().then((res) => tweet("xyz", id));
+};
+
+const tweet = async (text, repid) => {
+  var uppath = "./" + repid + "example.png";
+
+  try {
+    const mediaIds = await Promise.all([rwClient.v1.uploadMedia(uppath)]);
+    await rwClient.v1.reply("here is your SS", repid, {
+      media_ids: mediaIds,
+    });
+    console.log("ss sent");
+    fs.unlinkSync(uppath);
+  } catch (e) {
+
+    uppath = "./errorImg2.jpeg";
+    const mediaIds = await Promise.all([rwClient.v1.uploadMedia(uppath)]);
+    await rwClient.v1.reply("Unable to create SS", repid, {
+      media_ids: mediaIds,
+    });
+
+    console.error(e);
+  }
+};
+
+ 
+
+// const job = new CronJob("30 * * * * *", () => {
+//   console.log("cron job starting!");
+//   //tweet()
+//   main();
+// });
+// //
+// console.log("at job");
+// job.start();
+// //main();
+
+app.post('/some_route',async (req, res) => {
+  await main();
+  return res.send('ok')
+}); 
+
+const hostname = "0.0.0.0";
+const port = 3000;
+
+app.listen(port, hostname, () => {
+  console.log("Server running at http://$(hostname):$(port)/");
+});
